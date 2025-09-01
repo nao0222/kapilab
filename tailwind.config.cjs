c02270f8d977ab5d993ad7322dc54fbd0ca6a070
かすみん <hayama.k7@gmail.com>
2025-09-01 11:18:22 +0900

    エラー修正

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,ts,jsx,tsx}'], // <- 必須
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
