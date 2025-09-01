b01fe4dcad836936246c7ee0cdb4bd99bef8e76c
かすみん <hayama.k7@gmail.com>
2025-09-01 11:02:51 +0900

    エラー修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import cloudflare from '@astrojs/cloudflare';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  vite: {
-    build: {
-      cssCodeSplit: true, // CSS を JS から分離してファイル化
-    },
-  },
+  // vite: {
+  //   build: {
+  //     cssCodeSplit: true, // CSS を JS から分離してファイル化
+  //   },
+  // },
   site: 'https://kapilab.jp',
   base: '/',
-  adapter: cloudflare(),
 });
